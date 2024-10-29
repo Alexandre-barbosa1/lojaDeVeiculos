dac1cc4fd97783a369483a3f14f00b34e28eebcf
alexandre <josealexandre2005@outlook.com>
2024-10-29 16:24:48 -0300

    2 versao

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,21 +11,53 @@ public class Program {
 
 	public static void main(String[] args) {
 		Loja loja = new Loja();
-		Scanner sc = new Scanner (System.in);
+		Veiculo carro = null;
+		Veiculo moto = null;
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Loja");
-		Veiculo carro = new Carro("ferrari","A4","1993",40.000,2,"eletrico");
-		Veiculo moto = new Moto("honda","3A","1994",20.000,4,true);
-		loja.adicionar(carro);
-		loja.adicionar(moto);
-		System.out.println("veiculos disponiveis");
+		System.out.print("quantos veiculos deseja adicionar?");
+		int n = sc.nextInt();
+		sc.nextLine();
+		for (int i = 0; i < n; i++) {
+			System.out.print("adicione a marca do veiculo:  ");
+			String marca = sc.nextLine();
+			System.out.print("aidicione o modelo do veiculo: ");
+			String modelo = sc.nextLine();
+			System.out.print("adicione o ano de  fabricação: ");
+			String anoFabri = sc.nextLine();
+			System.out.print("adicione o preço do veiculo: ");
+			double preco = sc.nextDouble();
+			System.out.println("o seu veiculo é um carro ou uma moto? ");
+			sc.nextLine();
+			String r = sc.nextLine();
+			r = r.toLowerCase();
+			if (r.equals("carro")) {
+				System.out.println("adicione o numero de portas do carro: ");
+				Integer num = sc.nextInt();
+				System.out.println("qual o tipo de compustivel? ");
+				sc.nextLine();
+				String combustivel = sc.next();
+				carro = new Carro(marca, modelo, anoFabri, preco, num, combustivel);
+				loja.adicionar(carro);
+			} else {
+				System.out.println("Adicione o numero de cilindradas: ");
+				Integer cilin = sc.nextInt();
+				moto = new Moto(marca, modelo, anoFabri, preco, cilin, true);
+				loja.adicionar(moto);
+
+			}
+			sc.nextLine();
+		}
+
+		System.out.println("veiculos disponiveis: ");
 		loja.retorno();
-		System.out.println("carros disponiveis");
+		System.out.println();
+		System.out.println("carros disponiveis: ");
 		loja.listCarros();
-		System.out.println("motos disponiveis");
+		System.out.println();
+		System.out.println("motos disponiveis: ");
 		loja.listMotos();
-		
-		
-		
+
 		sc.close();
 
 	}
